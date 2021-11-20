fe2582759807afc964662af50d1f78560b2ca90a
HugoArriojas <hugo.arriojas@hotmail.com>
2021-11-19 23:15:51 -0500

    added category filter function, and hover descriptions over items

--- src/App.js
+++ src/App.js
@@ -7,54 +7,85 @@ function App() {
 
   // using useState in order to hold the items from the Fake store API
   const [item, setItem] = useState([])
+  // Holding selected category
+  const [categoryInput, setCategoryInput] = useState("");
+
+  // setting category input to null so that all items appear on page load
+  // const categoryInput = "category/electronics";
+
+  const categoryChange = (select) => {
+    setCategoryInput(select);
+  }
 
 
-  
   useEffect(() => {
     // Calling the API using Axios
-    axios ({
+    axios({
       method: "GET",
-      url: "https://fakestoreapi.com/products",
+      url: `https://fakestoreapi.com/products/${categoryInput}`,
       responseType: "json",
-      // params: {
-        //   category: null,
-        // }
-      })
+    })
+    
       .then((response) => {
         console.log(response.data)
         // using useState in order to store the received product array
         setItem(response.data)
       })
-      
-    }, [])
-    // We want API call to be made once and only on page load, therefore we make an empty dependency array at the end of useEffect
+
+  }, [categoryInput])
+  // We want API call to be made once and only on page load, therefore we make an empty dependency array at the end of useEffect
 
 
   return (
     <div className="App">
-      <header>
 
-      <h1>Reactive <span>Retail</span></h1>
-      <div className="imageTriangle"></div>
+
+      <header>
+        <h1>Reactive <span>Retail</span></h1>
+        <div className="imageTriangle"></div>
       </header>
-      <div className="productContainers">
-      {
-        item.map((product) => {
-          console.log(product)
-          return (
-            <ShowProduct 
-            key={product.id} 
-            image={product.image} 
-            title={product.title}
-            price={product.price}
-            rating={product.rating.rate}
-            count={product.rating.count}
-            description={product.description}
-            />
+
+      <section className="sideNav">
+        <h2>Sort by:</h2>
+        <nav>
+          <h3>Categories</h3>
+          <ul>
+            {/* How do I revert back to none?? */}
+            <li><button onClick={() => categoryChange("")}>All</button></li>
+            <li><button onClick={() => categoryChange("category/electronics")}>Electronics</button></li>
+            <li><button onClick={() => categoryChange("category/jewelery")}>Jewelry</button></li>
+            <li><button onClick={() => categoryChange("category/men's%20clothing")}>Men's clothing</button></li>
+            <li><button onClick={() => categoryChange("category/women's%20clothing")}>Women's clothing</button></li>
+          </ul>
+          <h3>Rating</h3>
+          <ul>
+            <li>1+</li>
+            <li>2+</li>
+            <li>3+</li>
+            <li>4+</li>
+          </ul>
+        </nav>
+
+      </section>
+
+      <section className="productContainers">
+        {
+          item.map((product) => {
+            // console.log(product)
+            return (
+              <ShowProduct
+                key={product.id}
+                image={product.image}
+                title={product.title}
+                price={product.price}
+                rating={product.rating.rate}
+                count={product.rating.count}
+                description={product.description}
+              />
             )
           })
         }
-        </div>
+      </section>
     </div>
 
   );
--- src/ShowProduct.js
+++ src/ShowProduct.js
@@ -1,13 +1,28 @@
 // Props here represent the props that we passed in App.js
 //ie. key, image, alt(?), title, price, rating, description, count
+// For the hover function
+import React, {useState} from "react";
+
 
 function ShowProduct(props) {
+
+    const [style, setStyle] = useState({display: 'none'});
+
+
     return (
         <div key={props.key} class="itemContainer">
-            <div className="itemImage">
+            <div className="itemImage"
+            onMouseEnter={event => {
+                setStyle({display: 'block'});
+            }}
+            onMouseLeave={event => {
+                setStyle({display: 'none'})
+            }}
+            >
+                {/* 🚨🚨🚨 What does style actually do here? */}
+                <p class="itemDesc" style={style}>{props.description}</p>
                 <img src={props.image} alt={props.title} />
                 {/* 🚨 What is a good alt caption for this so it's not just the title?? Potentially using the category?? */}
-                <p class="itemDesc">{props.description}</p>
             </div>
             <div className="itemText">
                 <h3 class="itemTitle">{props.title}</h3>
