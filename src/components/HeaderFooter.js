27ca29864db43655d7a16ccbaa473e677f846235
HugoArriojas <hugo.arriojas@hotmail.com>
2021-12-30 22:30:10 -0500

    testing if production branch is updating

--- src/components/HeaderFooter.js
+++ src/components/HeaderFooter.js
@@ -1,7 +1,7 @@
 const HeaderFooter = (props) => {
     return (
     <div className="header footer">
-        <h1>Reactive <span>Retail</span></h1>
+        <h1>Reactive <span>Retailzz</span></h1>
         <div className="imageTriangle"></div>
       </div>
     )
